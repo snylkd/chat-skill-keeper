0282592d756e2caed242aaf813526e3ea743fd5b
Yanis.INFOTEL <yanis.laakad@infotel.com>
2025-02-18 16:15:50 +0100

    Create srcindex.tsx

---
+++ srcindex.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const rootElement = document.getElementById("root")!;
+createRoot(rootElement).render(<App />);
