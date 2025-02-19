024a9856c3d15906c589fcbed3d92765ace9bf45
Yanis.INFOTEL <yanis.laakad@infotel.com>
2025-02-19 14:16:47 +0100

    Create s.ts

---
+++ s.ts
@@ -0,0 +1,9 @@
+// src/types/react-com-client.d.ts
+declare module 'react-com/client' {
+  // Si tu connais les types du module, tu peux les déclarer ici.
+  // Par exemple :
+  export function someFunction(arg: string): void;
+  // Ou tu peux simplement déclarer le module de manière générique :
+  const value: any;
+  export = value;
+}
